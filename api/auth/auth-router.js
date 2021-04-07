83733fd53e4019ec0d7e5a8275df0e1bdef0abdf
ChadDiaz <chadcdiaz@gmail.com>
2021-04-06 23:42:06 -0400

    working the lecture

---
+++ api/auth/auth-router.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const Users = require("../users/users-model.js");
+const bcrypt = require("bcryptjs");
+
+router.post("/register", async (req, res, next) => {
+  try {
+    const newUser = req.body;
+    const hash = bcrypt.hashSync(newUser.password, 10);
+    newUser.password = hash;
+    const saved = await Users.add(newUser);
+    res.status(200).json(saved);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    let { username, password } = req.body;
+    const user = await Users.findBy({ username }).first();
+    if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.user = user;
+      res.status(200).json({ message: `welcome ${user.username}` });
+    } else {
+      res.status(401).json({ message: "invalid creds" });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/logout", async (req, res, next) => {
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) {
+        res.send("You can checkout anytime you like...");
+      } else {
+        res.send("so long and thanks for all the fish...");
+      }
+    });
+  } else {
+    res.end();
+  }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,22 +1,48 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
+const loggedInCheck = require("./auth/logInCheckMiddlware.js");
+
+const sessionConfig = {
+  name: "chads session cookie",
+  secret: "i have the best cats",
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    secure: false, // NOTE this is normally true if you are using https
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    knex: require("../database/connection.js"),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 60 * 60 * 1000,
+  }),
+};
 
 const usersRouter = require("./users/users-router.js");
+const authRouter = require("./auth/auth-router.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
-server.use("/api/users", usersRouter);
+server.use("/api/users", loggedInCheck, usersRouter);
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use((err, req, res, next) => {
+  // eslint-disable-line
   res.status(500).json({
     message: err.message,
     stack: err.stack,
